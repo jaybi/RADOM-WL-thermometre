e5db44341705578cf54aa9a6ebc6ade601a179a5
jbsapis <jbsapis@gmail.com>
2020-01-20 15:10:01 +0100

    update

--- .circleci/install_arduino.sh
+++ .circleci/install_arduino.sh
@@ -5,10 +5,16 @@ yes | sudo apt install python-pip
 pip install pyserial
 pip install --upgrade pip
 
+echo " > wget -O arduino-cli-linux64.tar.gz https://github.com/arduino/arduino-cli/releases/download/0.7.2/arduino-cli_0.7.2_Linux_64bit.tar.gz"
 wget -O arduino-cli-linux64.tar.gz https://github.com/arduino/arduino-cli/releases/download/0.7.2/arduino-cli_0.7.2_Linux_64bit.tar.gz
+
+echo " > tar -zxvf arduino-cli-linux64.tar.gz"
 tar -zxvf arduino-cli-linux64.tar.gz
 
+echo " > sudo mv arduino-cli /usr/local/share/arduino-cli"
 sudo mv arduino-cli /usr/local/share/arduino-cli
+
+echo " > sudo ln -s /usr/local/share/arduino-cli /usr/local/bin/arduino-cli"
 sudo ln -s /usr/local/share/arduino-cli /usr/local/bin/arduino-cli
 
 printf "board_manager:
@@ -17,4 +23,4 @@ printf "board_manager:
 sudo mv .cli-config.yml /usr/local/share/
 
 arduino-cli core update-index
-arduino-cli core install esp32:esp32
+arduino-cli core install arduino:avr
